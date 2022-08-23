55646cfa82bb67496d8cc52316eca85bffabaf99
FiMcMahon <fionamcmahon@hotmail.com>
2022-08-23 17:56:05 +0000

    Save an array of highscores into local storage

--- end.js
+++ end.js
@@ -3,7 +3,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const totalScore = document.getElementById('totalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
 const MAX_HIGH_SCORES = 5;
+
 totalScore.innerText = mostRecentScore;
 
 saveHighScore = (e) => {

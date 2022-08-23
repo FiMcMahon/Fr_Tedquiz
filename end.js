fbf24c85300c207e1c59bbbe852cee6f82b4d382
FiMcMahon <fionamcmahon@hotmail.com>
2022-08-23 17:58:10 +0000

    Push highscores to array and sort+splice scores to show top 5

--- end.js
+++ end.js
@@ -16,3 +16,10 @@ saveHighScore = (e) => {
         score: mostRecentScore,
         name: username.value,
     };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
+};

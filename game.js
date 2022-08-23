e0a9749e6a6b5387eb3d749f7c0cdb9bf36fb6b9
FiMcMahon <fionamcmahon@hotmail.com>
2022-08-23 17:43:14 +0000

    Add fxn to update progress bar

--- game.js
+++ game.js
@@ -30,6 +30,9 @@ getNewQuestion = () => {
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];

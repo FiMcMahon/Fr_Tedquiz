9519be2fa274c5fc5ccea0da26f81f8290cd6cac
FiMcMahon <fionamcmahon@hotmail.com>
2022-08-23 17:54:32 +0000

    Add fxn to get highscores and saving most recent scores

--- end.js
+++ end.js
@@ -1,1 +1,15 @@
+const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const totalScore = document.getElementById('totalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const MAX_HIGH_SCORES = 5;
+totalScore.innerText = mostRecentScore;
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
